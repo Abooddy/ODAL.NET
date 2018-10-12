cdb692ccda03a2e28bc24218bfe5c34f406d3cf1
Abdullah Adel <abdullah.adel.3892@gmail.com>
2018-10-12 23:12:48 +0300

    Delete IClientSession.cs

--- C#/Infrastructure/IClientSession.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ODAL.Infrastructure
-{
-    public interface IClientSession
-    {
-        int UserID { get; set; }
-        string IP { get; set; }
-        string AccessToken { get; set; }
-    }
-}
